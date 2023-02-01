1815c2d7004511ba4873e141f6d6282556821cea
Mahmoud Martini <mahmoud5martini@gmail.com>
2023-02-01 21:46:08 +0300

    Files rearranged to MVC structure

---
+++ src/model/JDBCDemo.java
@@ -0,0 +1,29 @@
+package model;
+import model.Employee;
+import java.sql.*;
+
+
+public class JDBCDemo 
+{
+    
+    public static void main(String[] args) 
+    {
+        Employee employee;
+        try 
+        {
+            Class.forName("org.sqlite.JDBC");
+            Connection con = DriverManager.getConnection("jdbc:sqlite:Iyad.db");
+            Statement statement = con.createStatement();
+            ResultSet res = statement.executeQuery("SELECT * FROM Employee;");
+
+            while (res.next())
+            {
+                employee = new Employee(res);
+                System.out.println(employee.toString());
+            }
+        } 
+          catch (ClassNotFoundException | SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+}
